faacbf9271c279894ef449504fcacc91ae6313a7
El pepe <andresavilamf@gmail.com>
2023-10-23 20:33:44 -0300

    End hook useState

--- hook-useState/src/App.jsx
+++ hook-useState/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+    /* El valor inicial es 0 */
+    const [counter, setCounter] = useState(0);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    // const handleAdd = () => {
+    //     setCounter(counter + 1);
+    // };
+
+
+    //Otra manera de hacerlo
+    const handleAdd = () => {
+        setCounter((c)  => c+1)
+    }
+
+
+    return (
+        <>
+            <h1>CounterApp</h1>
+            <h2>{counter}</h2>
+            <button onClick={handleAdd}>+1</button>
+        </>
+    );
 }
 
-export default App
+export default App;
